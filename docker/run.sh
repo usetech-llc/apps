6e69d9ae45712fe3482bd5c4fbed956d3589948e
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-04-27 15:10:38 +0300

    Add docker files

---
+++ docker/run.sh
@@ -0,0 +1,11 @@
+mkdir -p /chain-data
+
+if [ -d "/chain-data/chains" ]
+then
+    chmod 777 -R /chain-data
+fi
+
+# dump local chain spec
+cargo run --release -- build-spec --dev --chain=local > /chain-data/chainspec.json
+
+cargo run --release -- --ws-external --rpc-external --dev --base-path /chain-data --chain=chainspec.json
